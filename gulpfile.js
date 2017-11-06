11ea48d192ab3314af77f11145d4c9d4d3c14b07
jonathanlevaillant <jonathan.levaillant@outlook.com>
2017-11-06 14:39:22 +0100

    update gulpfile config

--- gulpfile.js
+++ gulpfile.js
@@ -1,168 +1,134 @@
-// requires
-var gulp = require('gulp');
+const gulp = require('gulp');
+const util = require('gulp-util');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const uglify = require('gulp-uglify');
+const stylelint = require('gulp-stylelint');
+const eslint = require('gulp-eslint');
+const sourcemaps = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');
 
-// include plugins
-var plugins = require('gulp-load-plugins')();
-var run = require('run-sequence');
-var browserify = require('browserify');
-var babelify = require('babelify');
-var stream = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-
-// paths root
-var source = './src/';
-var destination = './dist/';
-
-// paths files
-var html = '**/*.html';
-var scss = 'scss/**/*.+(scss|sass|css)';
-var js = 'js/**/*.js';
-var app = 'js/app.js';
-var images = 'img/**/*.+(png|jpg|jpeg|gif|svg)';
-var fonts = 'fonts/**/*';
-var cssmin = 'css/main.min.css';
-var jsmin = 'js/app.min.js';
-
-/* linters = 'stylelint' + 'eslint'
+/* config
  ========================================================================== */
 
-// task 'stylelint' = stylelint (source -> console)
-gulp.task('stylelint', function() {
-  return gulp.src(source + scss)
-    .pipe(plugins.stylelint({
+// paths
+const paths = {
+  entry: './src/assets/',
+  output: './dist/',
+  app: 'scripts/main.js',
+  styles: 'styles/**/*.+(scss|sass|css)',
+  scripts: 'scripts/**/*.js',
+  images: 'images/**/*.+(png|jpg|jpeg|gif|svg)',
+  fonts: 'fonts/**/*.+(eot|svg|ttf|woff|woff2)'
+};
+
+// environments
+const production = !!util.env.env;
+
+/* linters (stylelint, eslint)
+ ========================================================================== */
+
+// task 'stylelint'
+gulp.task('stylelint', () =>
+  gulp.src(paths.entry + paths.styles)
+    .pipe(stylelint({
       reporters: [{
         formatter: 'string',
         console: true
       }]
     }))
-});
+);
 
-// task 'eslint' = eslint (source -> console)
-gulp.task('eslint', function() {
-  return gulp.src(source + js)
-    .pipe(plugins.eslint({
+// task 'eslint'
+gulp.task('eslint', () =>
+  gulp.src(paths.entry + paths.scripts)
+    .pipe(eslint({
       configFile: '.eslintrc.json'
     }))
-    .pipe(plugins.eslint.format())
-});
+    .pipe(eslint.format())
+);
 
-/* task 'build' = ['html' + 'css' + 'js' + 'img' + 'fonts']
-   ========================================================================== */
+/* build (css, js, images, fonts)
+ ========================================================================== */
 
-// task 'html' = copy paste (source -> destination)
-gulp.task('html', function() {
-  return gulp.src(source + html)
-    .pipe(gulp.dest(destination))
-});
-
-// task 'css' = stylelint + sass + autoprefixer + cssbeautify (source -> destination)
-gulp.task('css', ['stylelint'], function() {
-  return gulp.src(source + scss)
-    .pipe(plugins.sass({
-      errLogToConsole: true,
+// task 'css'
+gulp.task('css', ['stylelint'], () =>
+  gulp.src(paths.entry + paths.styles)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
       outputStyle: 'expanded'
-    })
-    .on('error', plugins.sass.logError))
-    .pipe(plugins.autoprefixer({
-      browsers: ['> 1%', 'last 2 versions', 'Firefox ESR'],
+    }).on('error', sass.logError))
+    .pipe(autoprefixer({
       cascade: false
     }))
-    .pipe(plugins.cssbeautify({
-      indent: '  ',
-    }))
-    .pipe(gulp.dest(destination + 'css/'))
-});
+    .pipe(production ? cssnano() : util.noop())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.output + 'css/'))
+);
 
-// task 'js' = eslint + babel transpiler es6 to es5 (source -> destination)
-gulp.task('js', ['eslint'], function() {
-  var bundler = browserify({
-    entries: source + app,
-    debug: true,
-    transform: [babelify.configure({
-      presets: ['es2015'],
-      plugins: ['transform-object-rest-spread']
-    })]
-  });
-  bundler.bundle()
-    .on('error', function (err) {})
-    .pipe(stream('app.js'))
+// task 'js'
+gulp.task('js', ['eslint'], () => {
+  browserify({
+      entries: paths.entry + paths.app,
+      debug: true
+    })
+    .transform('babelify')
+    .bundle()
+    .on('error', err => {
+      console.error(`${err.message}${err.codeFrame}`)
+    })
+    .pipe(source('main.js'))
     .pipe(buffer())
-    .pipe(gulp.dest(destination + 'js/'))
+    .pipe(sourcemaps.init({
+      loadMaps: true
+    }))
+    .pipe(production ? uglify() : util.noop())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.output + 'js/'))
 });
 
-// task 'img' = copy paste (source -> destination)
-gulp.task('img', function() {
-  return gulp.src(source + images)
-    .pipe(gulp.dest(destination + 'img/'))
-});
-
-// task 'fonts' = copy paste (source -> destination)
-gulp.task('fonts', function() {
-  return gulp.src(source + fonts)
-    .pipe(gulp.dest(destination + 'fonts/'))
-});
-
-// task 'build'
-gulp.task('build', function(callback) {
-  run(['html', 'css', 'js', 'img', 'fonts'], callback)
-});
-
-/* task 'prod' = 'url' + ['cssmin' + 'jsmin' + 'imgmin']
-   ========================================================================== */
-
-// task 'url' = useref (destination -> destination)
-gulp.task('url', function() {
-  return gulp.src(destination + html)
-    .pipe(plugins.useref())
-    .pipe(gulp.dest(destination))
-});
-
-// task 'cssmin' = cssnano (destination -> destination)
-gulp.task('cssmin', function() {
-  return gulp.src(destination + cssmin)
-    .pipe(plugins.cssnano())
-    .pipe(gulp.dest(destination + 'css/'))
-});
-
-// task 'jsmin' = uglify (destination -> destination)
-gulp.task('jsmin', function() {
-  return gulp.src(destination + jsmin)
-    .pipe(plugins.uglify())
-    .pipe(gulp.dest(destination + 'js/'))
-});
-
-// task 'imgmin' = imagemin (destination -> destination)
-gulp.task('imgmin', function() {
-  return gulp.src(destination + images)
-    .pipe(plugins.imagemin([
-      plugins.imagemin.gifsicle({
+// task 'images'
+gulp.task('images', () =>
+  gulp.src(paths.entry + paths.images)
+    .pipe(production ? imagemin([
+      imagemin.gifsicle({
         interlaced: true
       }),
-      plugins.imagemin.jpegtran({
+      imagemin.jpegtran({
         progressive: true
       }),
-      plugins.imagemin.svgo({plugins: [{
-        removeUnknownsAndDefaults: false
-      }]})
-    ]))
-    .pipe(gulp.dest(destination + 'img/'))
+      imagemin.svgo({
+        plugins: [{
+          removeUnknownsAndDefaults: false
+        }]
+      })
+    ]) : util.noop())
+    .pipe(gulp.dest(paths.output + 'images/'))
+);
+
+// task 'fonts'
+gulp.task('fonts', () =>
+  gulp.src(paths.entry + paths.fonts)
+    .pipe(gulp.dest(paths.output + 'fonts/'))
+);
+
+// build
+gulp.task('build', ['css', 'js', 'images', 'fonts']);
+
+/* watch (css, js)
+ ========================================================================== */
+
+gulp.task('watch', () => {
+  gulp.watch(paths.entry + paths.styles, ['css']);
+  gulp.watch(paths.entry + paths.scripts, ['js']);
 });
 
-// task 'prod'
-gulp.task('prod', function(callback) {
-  run('url', ['cssmin', 'jsmin', 'imgmin'], callback)
-});
-
-/* task 'watch' = 'css' + 'html' + 'js'
-   ========================================================================== */
-
-gulp.task('watch', function() {
-  gulp.watch(source + html, ['html']);
-  gulp.watch(source + scss, ['css']);
-  gulp.watch(source + js, ['js']);
-});
-
-/* task 'default' = 'build'
-   ========================================================================== */
+/* default (build)
+ ========================================================================== */
 
 gulp.task('default', ['build']);
